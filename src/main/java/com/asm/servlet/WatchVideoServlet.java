d45fd9e72c4d70edc80e0a937238a0faa1eed152
DevHieu <hieudd2090@gmail.com>
2025-12-02 21:46:12 +0700

    Làm được chức năng lưu lịch sử xem

--- src/main/java/com/asm/servlet/WatchVideoServlet.java
+++ src/main/java/com/asm/servlet/WatchVideoServlet.java
@@ -1,20 +1,42 @@
 package com.asm.servlet;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.asm.dao.HistoryDAO;
+import com.asm.dao.impl.HistoryDAOImpl;
+import com.asm.entity.History;
+import com.asm.entity.User;
 
 @WebServlet("/watch")
 public class WatchVideoServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
+  private HistoryDAO historyDAO = new HistoryDAOImpl();
+
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    HttpSession session = request.getSession(true);
     String id = request.getParameter("id");
-    System.out.println(id);
+    User user = (User) session.getAttribute("user");
+
+    History history = historyDAO.existsByUserIdAndVideoId(user.getId(), id);
+    if (history == null) {
+      history = new History();
+      history.setUserId(user.getId());
+      history.setVideoId(id);
+      historyDAO.create(history);
+    } else {
+      history.setViewDate(new Date());
+      historyDAO.update(history);
+    }
+
     request.getRequestDispatcher("/views/videoPage.jsp").forward(request, response);
   }
 
